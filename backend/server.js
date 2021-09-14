6062de962c547201d25774c2a4f04dbf42b399b9
stephanos50 <stefan.arvanitis@yahoo.fr>
2021-09-14 16:06:08 +0200

    build

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')))
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
