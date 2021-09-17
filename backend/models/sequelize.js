50f6e29e3f7d9eff8433167fd3d4a663b4274ca0
stephanos50 <stefan.arvanitis@yahoo.fr>
2021-09-17 16:18:22 +0200

    config

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -1,5 +1,5 @@
-const config = require("../config/mysql.json");
-//const config = require("../config/postgres.json");
+//const config = require("../config/mysql.json");
+const config = require("../config/postgres.json");
 
 const debug = require("debug")("projet-stephanos50:sequelize");
 const { Sequelize } = require("sequelize");
@@ -10,10 +10,16 @@ const sequelize = new Sequelize(
   config.password,
   {
     logging: (msg) => debug(msg),
-    dialect: "mysql",
+    dialect: "postgres",
+    protocol: 'postgres',
     host: config.host,
     port: config.port,
-
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // 
+      }
+    }
   }
 );
 
